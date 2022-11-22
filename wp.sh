f60a67e8885644bdf7e6469b855dfbb2ff23b8fe
dnoizeshakerz <40494966+dnoizeshakerz@users.noreply.github.com>
2022-11-22 19:37:25 +0800

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -15,7 +15,7 @@ if [ "$run" == n ] ; then
 exit
 else
 echo "============================================"
-echo "A robot is now installing WordPress for you."
+echo "Berdoalah kepada tuhan"
 echo "============================================"
 #download wordpress
 curl -O https://wordpress.org/latest.tar.gz
@@ -35,9 +35,6 @@ cp wp-config-sample.php wp-config.php
 perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
 perl -pi -e "s/username_here/$dbuser/g" wp-config.php
 perl -pi -e "s/password_here/$dbpass/g" wp-config.php
-#create uploads folder and set permissions
-mkdir wp-content/uploads
-chmod 755 wp-content/uploads
 #remove zip file
 rm latest.tar.gz
 #remove bash script
